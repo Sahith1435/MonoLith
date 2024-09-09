d1dcb12d690564d857e77c3fe1cda36570db3806
Sahith <98472740+Sahith1435@users.noreply.github.com>
2024-09-09 11:56:57 +0530

    Backend file removed

--- s3.tf
+++ s3.tf
@@ -22,11 +22,4 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafaccit.devops.project.bucket"
-key = "prod/terraform.tfstate"
-}
 }
